ce2a76123bd187f03293fe6adb8adb02177a609f
Nikunj Jadav <dirensj@gmail.com>
2026-05-17 14:15:49 +0530

    feat(document): add document upload API with validation

--- app/api/v1/routes.py
+++ app/api/v1/routes.py
@@ -1,5 +1,12 @@
 from fastapi import APIRouter
 from app.api.v1.health_routes import router as health_router
+from app.api.v1.document_routes import router as document_router
+
 
 api_router = APIRouter()
 api_router.include_router(health_router,tags=["Health"])
+api_router.include_router(
+    document_router,
+    prefix="/documents",
+    tags=["Documents"]
+)
